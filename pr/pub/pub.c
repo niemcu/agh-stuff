11d527e4d3fde70723ebe844f2605ce59cb66917
niemcu <szniemcu@gmail.com>
2015-11-12 14:26:13 +0100

    macie chlopaki XDclear

--- pr/pub/pub.c
+++ pr/pub/pub.c
@@ -14,7 +14,45 @@ pthread_mutex_t mug_mutex;
 pthread_mutex_t bar_mutex;
 //watek albo czeka na napelnienie, albo pije
 
+pthread_cond_t mugs_cond;
 
+void *visit_pub_cond(unsigned int *arg) {
+	unsigned int th_num = *arg;
+    int success = 0;
+    int k;
+    printf("Moje ID %d; Wchodze do pubu\n", th_num);
+    for (k = 1; k < 3; k++) {
+    	printf("Moje ID %d; Chce pobrac kufel %d \n", th_num, k);
+		for (;;) {
+			pthread_mutex_lock(&mug_mutex);
+			if (my_pub.mugs > 0) {
+				success = 1;
+				my_pub.mugs--;
+			}
+			pthread_mutex_unlock(&mug_mutex);
+			if (success == 1) {
+				success = 0;
+				printf("Moje ID %d; Mam kufel, czekam na kran\n", th_num);
+				pthread_mutex_lock(&bar_mutex);
+				printf("Moje ID %d; Mam kran, nalewam piwo\n", th_num);
+					sleep(3);
+				pthread_mutex_unlock(&bar_mutex);
+				// picie piwa
+				printf("Moje ID %d; Pije piwo\n", th_num);
+				sleep(10);
+				printf("Moje ID %d; Wypilem. oddaje kufel\n", th_num);
+				pthread_mutex_lock(&mug_mutex);
+				my_pub.mugs++;
+				pthread_mutex_unlock(&mug_mutex);
+				break;
+			} else {
+				pthread_cond_wait(*mugs_cond);
+			}	
+		}
+	}
+	printf("Moje ID %d; Upilem sie. Wychodze z baru.\n", th_num);
+    return NULL;
+}
 
 void *visit_pub(unsigned int *arg) {
     unsigned int th_num = *arg;
