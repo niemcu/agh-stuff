b9d08e1bac050b9165b1a13fbd65abfcdff1f7da
Szymon Niemiec <szniemcu@gmail.com>
2015-12-03 09:11:20 +0100

    cos mi nie idzie z tym

--- pr/lab910/openmp_petle.c
+++ pr/lab910/openmp_petle.c
@@ -7,33 +7,41 @@
 
 main ()
 {
-  double a[WYMIAR][WYMIAR];
-  int n,i,j;
+    double a[WYMIAR][WYMIAR];
+    int n,i,j;
 
-  for(i=0;i<WYMIAR;i++) for(j=0;j<WYMIAR;j++) a[i][j]=1.02*i+1.01*j;
+    for(i=0;i<WYMIAR;i++) for(j=0;j<WYMIAR;j++) a[i][j]=1.02*i+1.01*j;
 
-  n=WYMIAR;
+    n=WYMIAR;
 
-  double suma=0.0;
-  for(i=0;i<WYMIAR;i++) {
-    for(j=0;j<WYMIAR;j++) {
-      suma += a[i][j];
+    double suma=0.0;
+    for(i=0;i<WYMIAR;i++) {
+        for(j=0;j<WYMIAR;j++) {
+            suma += a[i][j];
+        }
     }
-  }
-  
-  printf("Suma wyrazów tablicy: %lf\n", suma);
 
-  omp_set_nested(1);
-  omp_set_num_threads(5);
-  double suma_parallel=0.0;
-  #pragma omp parallel for private(i, j)
-  for(i=0;i<WYMIAR;i++) {
-    for(j=0;j<WYMIAR;j++) {
-      suma_parallel += a[i][j];
-      printf("thread %d, i %d, j %d\n", omp_get_thread_num(), i, j);
+    printf("Suma wyrazów tablicy: %lf\n", suma);
+
+    omp_set_nested(1);
+    omp_set_num_threads(15);
+    double suma_parallel=0.0;
+    //podzial wierszowy
+    //#pragma omp parallel for default(none) shared(suma_parallel, a) private(i, j)
+    //for(i=0;i<WYMIAR;i++) {
+    //  for(j=0;j<WYMIAR;j++) {
+    //    suma_parallel += a[i][j];
+    //    printf("thread %d, i %d, j %d\n", omp_get_thread_num(), i, j);
+    //  }
+    //}
+    //podzial kolumnowy
+    #pragma omp parallel for collapse(2) default(none) shared(a) private(i, j) reduction(+:suma_parallel)
+    for(i=0;i<WYMIAR;i++) {
+        for(j=0;j<WYMIAR;j++) {
+            suma_parallel += a[i][j];
+            printf("thread %d, i %d, j %d\n", omp_get_thread_num(), i, j);
+        }
     }
-  }
-
-  printf("Suma wyrazów tablicy równolegle: %lf\n", suma_parallel);
+    printf("Suma wyrazów tablicy równolegle: %lf\n", suma_parallel);
 
 }
